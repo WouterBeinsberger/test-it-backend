2f5bd51a9e2d7bedac078ce57de469dffe9f00f3
Wouter Beinsberger <wouterbeinsberger@pop-os.localdomain>
2022-06-02 23:18:31 +0200

    feat(about-us): added routes and data

---
+++ about_us/data/about_us.py
@@ -0,0 +1,23 @@
+ABOUT_US = {
+  "id": "1",
+  "sectionElementId": "aboutUsSection",
+  "title": "About us",
+  "aboutUsItemList": [
+    {
+      "itemElementId": "aboutUsItemOne",
+      "itemTitle": "lorem"
+    },
+    {
+      "itemElementId": "aboutUsItemTwo",
+      "itemTitle": "ipsum"
+    },
+    {
+      "itemElementId": "aboutUsItemThree",
+      "itemTitle": "dolor"
+    },
+    {
+      "itemElementId": "aboutUsItemFour",
+      "itemTitle": "sit"
+    }
+  ]
+}
