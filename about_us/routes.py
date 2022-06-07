af58675c111c06472d98ca8c53d504ea7d6924ce
Your Name <beinsbergerwouter@gmail.com>
2022-06-07 22:48:04 +0200

    feat(about-us): put and about class added

--- about_us/routes.py
+++ about_us/routes.py
@@ -1,8 +1,16 @@
 from .data.about_us import ABOUT_US
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
+from .about import About
 
 about_us = Blueprint("about_us", __name__)
+about_data = About()
+about_data.data = ABOUT_US
 
 @about_us.route("/", methods=["GET"])
 def about():
-  return jsonify(ABOUT_US)
+  return jsonify(about_data.data)
+
+@about_us.route("/", methods=["PUT"])
+def update_about():
+  about_data.data  = request.get_json()
+  return jsonify(about_data.data)
