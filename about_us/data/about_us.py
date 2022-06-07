2618b3f2ee7a0e65c3dd69dea5705c968292788c
Your Name <beinsbergerwouter@gmail.com>
2022-06-07 21:26:11 +0200

    refactor: about us data

--- about_us/data/about_us.py
+++ about_us/data/about_us.py
@@ -1,23 +1,41 @@
 ABOUT_US = {
-  "id": "1",
-  "sectionElementId": "aboutUsSection",
-  "title": "About us",
-  "aboutUsItemList": [
-    {
-      "itemElementId": "aboutUsItemOne",
-      "itemTitle": "lorem"
+  "id": "6df8ad76-e68c-11ec-8fea-0242ac120002",
+  "elementId": "aboutUsSection",
+  "content": {
+    "title": {
+      "value": "About us",
+      "isDisplayed": True,
+      "elementId": "aboutUsTitle"
     },
-    {
-      "itemElementId": "aboutUsItemTwo",
-      "itemTitle": "ipsum"
-    },
-    {
-      "itemElementId": "aboutUsItemThree",
-      "itemTitle": "dolor"
-    },
-    {
-      "itemElementId": "aboutUsItemFour",
-      "itemTitle": "sit"
+    "aboutUsItemList": {
+      "isDisplayed": True,
+      "elementId": "aboutUsList",
+      "values": [
+        {
+          "id": "7980daec-e68c-11ec-8fea-0242ac120002",
+          "elementId": "aboutUsItemOne",
+          "itemTitle": "lorem",
+          "isDisplayed": True
+        },
+        {
+          "id": "7e1ec884-e68c-11ec-8fea-0242ac120002",
+          "elementId": "aboutUsItemTwo",
+          "itemTitle": "ipsum",
+          "isDisplayed": True
+        },
+        {
+          "id": "8458a8aa-e68c-11ec-8fea-0242ac120002",
+          "elementId": "aboutUsItemThree",
+          "itemTitle": "dolor",
+          "isDisplayed": True
+        },
+        {
+          "id": "898fb23c-e68c-11ec-8fea-0242ac120002",
+          "elementId": "aboutUsItemFour",
+          "itemTitle": "sit",
+          "isDisplayed": True
+        }
+      ]
     }
-  ]
+  }
 }
