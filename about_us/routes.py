5b3175854f21777f33bbc499d6802606e2da6872
Your Name <beinsbergerwouter@gmail.com>
2022-06-08 22:57:42 +0200

    refactor(about-us): blueprint variable changed

--- about_us/routes.py
+++ about_us/routes.py
@@ -2,15 +2,15 @@ from .data.about_us import ABOUT_US
 from flask import Blueprint, jsonify, request
 from .about import About
 
-about_us = Blueprint("about_us", __name__)
+about_us_routes = Blueprint("about_us", __name__)
 about_data = About()
 about_data.data = ABOUT_US
 
-@about_us.route("/", methods=["GET"])
+@about_us_routes.route("/", methods=["GET"])
 def about():
   return jsonify(about_data.data)
 
-@about_us.route("/", methods=["PUT"])
+@about_us_routes.route("/", methods=["PUT"])
 def update_about():
   about_data.data  = request.get_json()
   return jsonify(about_data.data)
--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from flask import Flask
-from about_us.routes import about_us
+from about_us.routes import about_us_routes
 from hero.routes import hero_routes
+from flask import Flask
 
 app = Flask(__name__)
 
 app.register_blueprint(hero_routes, url_prefix="/hero")
-app.register_blueprint(about_us, url_prefix="/about-us")
+app.register_blueprint(about_us_routes, url_prefix="/about-us")
 
 if __name__ == "__main__":
   app.run()
