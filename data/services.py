f793348b43dadea71e645d84e8b0d25966f43671
Your Name <beinsbergerwouter@gmail.com>
2022-06-10 19:07:45 +0200

    service data added

---
+++ data/services.py
@@ -0,0 +1,77 @@
+SERVICES = {
+  "id": "3b506d6a-e8de-11ec-8fea-0242ac120002",
+  "elementId": "servicesSection",
+  "content": {
+    "title": {
+      "value": "Services",
+      "isDisplayed": True,
+      "elementId": "servicesTitle"
+    },
+    "servicesList": {
+      "isDisplayed": True,
+      "elementId": "servicesList",
+      "values": [
+        {
+          "id": "7980daec-e68c-11ec-8fea-0242ac120002",
+          "elementId": "serviceListItemOne",
+          "isDisplayed": True,
+          "title": {
+            "value": "senectus et netus et malesuada",
+            "elementId": "serviceListItemTitleOne",
+            "isDisplayed": True
+          },
+          "description": {
+            "value": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nulla pellentesque dignissim enim sit amet venenatis urna cursus.",
+            "elementId": "serviceListItemDescriptionOne",
+            "isDisplayed": True
+          }
+        },
+        {
+          "id": "3a97c566-e8df-11ec-8fea-0242ac120002",
+          "elementId": "serviceListItemTwo",
+          "isDisplayed": True,
+          "title": {
+            "value": "diam phasellus vestibulum lorem sed",
+            "elementId": "serviceListItemTitleTwo",
+            "isDisplayed": True
+          },
+          "description": {
+            "value": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vitae sapien pellentesque habitant morbi tristique senectus.",
+            "elementId": "serviceListItemDescriptionTwo",
+            "isDisplayed": True
+          }
+        },
+        {
+          "id": "4b785ed6-e8df-11ec-8fea-0242ac120002",
+          "elementId": "serviceListItemThree",
+          "isDisplayed": True,
+          "title": {
+            "value": "quisque egestas diam in arcu",
+            "elementId": "serviceListItemTitleThree",
+            "isDisplayed": True
+          },
+          "description": {
+            "value": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sed cras ornare arcu dui vivamus arcu felis.",
+            "elementId": "serviceListItemDescriptionThree",
+            "isDisplayed": True
+          }
+        },
+        {
+          "id": "54e274fc-e8df-11ec-8fea-0242ac120002",
+          "elementId": "serviceListItemFour",
+          "isDisplayed": True,
+          "title": {
+            "value": "vestibulum sed arcu non odio",
+            "elementId": "serviceListItemTitleFour",
+            "isDisplayed": True
+          },
+          "description": {
+            "value": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Euismod elementum nisi quis eleifend quam adipiscing vitae proin.",
+            "elementId": "serviceListItemDescriptionFour",
+            "isDisplayed": True
+          }
+        }
+      ]
+    }
+  }
+}
