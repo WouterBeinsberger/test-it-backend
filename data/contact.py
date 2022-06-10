c2b82db1c7ab760fedbe5b729c9256bc65dc28e9
Your Name <beinsbergerwouter@gmail.com>
2022-06-10 19:40:29 +0200

    feat: contact data added

---
+++ data/contact.py
@@ -0,0 +1,38 @@
+CONTACT = {
+  "id": "1027b070-e8e4-11ec-8fea-0242ac120002",
+  "elementId": "conatctSection",
+  "input": {
+    "name": {
+      "isDisplayed": True,
+      "elementId": "contactInputName"
+    },
+    "email": {
+      "isDisplayed": True,
+      "elementId": "contactInputEmail"
+    },
+    "message": {
+      "isDisplayed": True,
+      "elementId": "contactInputMessage"
+    }
+  },
+  "content": {
+    "Address": {
+      "isDisplayed": True,
+      "elementId": "contactAddress",
+      "streetName": "Elenasteeg",
+      "streetNumber": "65",
+      "postalCode": "5586",
+      "city": "Rotstergaast"
+    },
+    "phoneNumber": {
+      "value": "+32477215939",
+      "isDisplayed": True,
+      "elementId": "contactPhoneNumber"
+    },
+    "email": {
+      "value": "info@testit.com",
+      "isDisplayed": True,
+      "elementId": "contactEmail"
+    }
+  }
+}
