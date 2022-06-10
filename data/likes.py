3bb44f95eee8ff926246d518cfa2f03ebeb06532
Your Name <beinsbergerwouter@gmail.com>
2022-06-10 19:13:00 +0200

    like data added

---
+++ data/likes.py
@@ -0,0 +1,16 @@
+LIKES = {
+  "id": "7f908418-e8e0-11ec-8fea-0242ac120002",
+  "elementId": "likeSection",
+  "content": {
+    "description": {
+      "value": "have liked our services",
+      "isDisplayed": True,
+      "elementId": "likeDescription"
+    },
+    "counter": {
+      "value": 23415,
+      "isDisplayed": True,
+      "elementId": "likeCounter"
+    }
+  }
+}
