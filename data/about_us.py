c00325b5009ad8feb8b6a1e834304878bf420c00
Your Name <beinsbergerwouter@gmail.com>
2022-06-21 22:03:39 +0200

    refactor(about): data restructured

--- data/about_us.py
+++ data/about_us.py
@@ -1,36 +1,46 @@
 ABOUT_US = {
   "id": "6df8ad76-e68c-11ec-8fea-0242ac120002",
-  "elementId": "aboutUsSection",
   "content": {
+    "section": {
+      "name": "About us",
+      "elementId": "aboutUsSection",
+      "isDisplayed": True,
+    },
     "title": {
+      "name": "Title",
       "value": "About us",
       "isDisplayed": True,
       "elementId": "aboutUsTitle"
     },
     "aboutUsItemList": {
+      "name": "About us list",
       "isDisplayed": True,
       "elementId": "aboutUsList",
       "values": [
         {
           "id": "7980daec-e68c-11ec-8fea-0242ac120002",
+          "name": "About us list item one",
           "elementId": "aboutUsItemOne",
           "itemTitle": "lorem",
           "isDisplayed": True
         },
         {
           "id": "7e1ec884-e68c-11ec-8fea-0242ac120002",
+          "name": "About us list item two",
           "elementId": "aboutUsItemTwo",
           "itemTitle": "ipsum",
           "isDisplayed": True
         },
         {
           "id": "8458a8aa-e68c-11ec-8fea-0242ac120002",
+          "name": "About us list item three",
           "elementId": "aboutUsItemThree",
           "itemTitle": "dolor",
           "isDisplayed": True
         },
         {
           "id": "898fb23c-e68c-11ec-8fea-0242ac120002",
+          "name": "About us list item four",
           "elementId": "aboutUsItemFour",
           "itemTitle": "sit",
           "isDisplayed": True
