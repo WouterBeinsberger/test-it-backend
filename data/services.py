cd3e4c77c4543082ad11516bf9c32f1018981c30
Your Name <beinsbergerwouter@gmail.com>
2022-06-29 19:28:53 +0200

    refactor(services): data restructured

--- data/services.py
+++ data/services.py
@@ -7,6 +7,7 @@ SERVICES = {
       "isDisplayed": True
     },
     "title": {
+      "name": "Title",
       "value": "Services",
       "isDisplayed": True,
       "elementId": "servicesTitle"
@@ -14,65 +15,78 @@ SERVICES = {
     "servicesList": {
       "isDisplayed": True,
       "elementId": "servicesList",
+      "name": "Services list",
       "values": [
         {
           "id": "7980daec-e68c-11ec-8fea-0242ac120002",
           "elementId": "serviceListItemOne",
           "isDisplayed": True,
+          "name": "Service card one",
           "title": {
             "value": "senectus et netus et malesuada",
             "elementId": "serviceListItemTitleOne",
-            "isDisplayed": True
+            "isDisplayed": True,
+            "name": "Service card one title"
           },
           "description": {
             "value": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nulla pellentesque dignissim enim sit amet venenatis urna cursus.",
             "elementId": "serviceListItemDescriptionOne",
-            "isDisplayed": True
+            "isDisplayed": True,
+            "name": "Service card one description"
           }
         },
         {
           "id": "3a97c566-e8df-11ec-8fea-0242ac120002",
           "elementId": "serviceListItemTwo",
           "isDisplayed": True,
+          "name": "Service card two",
           "title": {
             "value": "diam phasellus vestibulum lorem sed",
             "elementId": "serviceListItemTitleTwo",
-            "isDisplayed": True
+            "isDisplayed": True,
+            "name": "Service card two title"
           },
           "description": {
             "value": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vitae sapien pellentesque habitant morbi tristique senectus.",
             "elementId": "serviceListItemDescriptionTwo",
-            "isDisplayed": True
+            "isDisplayed": True,
+            "name": "Service card two description"
           }
         },
         {
           "id": "4b785ed6-e8df-11ec-8fea-0242ac120002",
           "elementId": "serviceListItemThree",
           "isDisplayed": True,
+          "name": "Service card three",
           "title": {
             "value": "quisque egestas diam in arcu",
             "elementId": "serviceListItemTitleThree",
-            "isDisplayed": True
+            "isDisplayed": True,
+            "name": "Service card three title"
           },
           "description": {
             "value": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sed cras ornare arcu dui vivamus arcu felis.",
             "elementId": "serviceListItemDescriptionThree",
-            "isDisplayed": True
+            "isDisplayed": True,
+            "name": "Service card three discription"
           }
         },
         {
           "id": "54e274fc-e8df-11ec-8fea-0242ac120002",
           "elementId": "serviceListItemFour",
           "isDisplayed": True,
+          "name": "Service card four",
           "title": {
             "value": "vestibulum sed arcu non odio",
             "elementId": "serviceListItemTitleFour",
-            "isDisplayed": True
+            "isDisplayed": True,
+            "name": "Service card four title"
           },
           "description": {
             "value": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Euismod elementum nisi quis eleifend quam adipiscing vitae proin.",
             "elementId": "serviceListItemDescriptionFour",
-            "isDisplayed": True
+            "isDisplayed": True,
+            "name": "Service card four description"
           }
         }
       ]
